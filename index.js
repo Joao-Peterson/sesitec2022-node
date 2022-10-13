565be23b19db212bd42df0704438122929c3b572
peterson <peterson.sheff@gmail.com>
2022-10-13 08:33:52 -0300

    cors

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const math = require("mathjs");
+const cors = require("cors");
 const app = express();
 
 app.listen(4000, () => {});
+app.use(cors())
 
 app.get("/esp", (req, res) => {
+    console.log("Connection from: [" + req.ip + "]");
+    
     res.send(
         {
-			current: math.round(math.random(0.2, 4), 2),
-			frequency: math.round(math.random(30, 60), 2)
+	    current: math.round(math.random(0.2, 4), 2),
+	    frequency: math.round(math.random(30, 60), 2)
         }
     )
 });
